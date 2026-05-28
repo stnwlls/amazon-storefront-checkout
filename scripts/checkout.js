b87476b50387f74ed9c62777e7f6c9cbffbbf977
Austin Wells <austinwells.dev@gmail.com>
2026-05-28 16:41:19 -0400

    Clean storefront source comments

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,66 +1,20 @@
 import {renderOrderSummary} from "./checkout/orderSummary.js";
 import {renderPaymentSummary} from "./checkout/paymentSummary.js";
-import {loadProducts, loadProductsFetch} from "../data/products.js";
+import {loadProductsFetch} from "../data/products.js";
 import {loadCart} from "../data/cart.js";
-// import '../data/cart-class.js';
-// import '../data/backend-practice.js';
 
 async function loadPage() {
   try {
-    // throw 'error1'
-
     await loadProductsFetch();
-    
-    const value = await new Promise((resolve, reject) => {
-      // throw 'error2';
-      loadCart(() => {
-        // reject('error3');
-        resolve('value3');
-      });
+    await new Promise((resolve) => {
+      loadCart(resolve);
     });
-    
   } catch (error) {
     console.error('An error occurred. Please try again later.');
   }
-  
+
   renderOrderSummary();
   renderPaymentSummary();
 }
+
 loadPage();
-
-// Promise.all([
-//   loadProductsFetch(),
-//   new Promise((resolve) => {
-  //     loadCart(() => {
-    //       resolve();
-//     });
-//   })
-
-// ]).then(() => {
-  //   renderOrderSummary();
-//   renderPaymentSummary();
-// });
-
-// new Promise((resolve) => {
-  //   loadProducts(() => {
-    //     resolve();
-//   });
-
-// }).then(() => {
-  //   return new Promise((resolve) => {
-    //     loadCart(() => {
-      //       resolve();
-//     });
-//   });
-
-// }).then(() => {
-  //   renderOrderSummary();
-//   renderPaymentSummary();
-// });
-
-// loadProducts(() => {
-  //   loadCart(() => {
-    //     renderOrderSummary();
-//     renderPaymentSummary();
-//   });
-// });
